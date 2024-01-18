183249ba46844f5bcbc45f3542563147d38919c7
kaybrian <kayonbrian@gmail.com>
2024-01-18 12:53:56 +0200

    little updates on the screens

--- hello_world/lib/main.dart
+++ hello_world/lib/main.dart
@@ -8,13 +8,42 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Hello World App'),
+          title:const Center(
+            child: Text('Hi, 👍',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          centerTitle: true,
+          shadowColor: Colors.black12,
+          backgroundColor: Colors.blueGrey[900],
+          elevation: 0.0,
+          leading: const Icon(
+            Icons.menu,
+            color: Colors.white,
+            size: 30.0,
+          ),
+
         ),
-        body: Center(
-          child: Text('Hello World!'),
+        body: const Center(
+          child: Text('Hello World!',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 30.0,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+
         ),
+
+        backgroundColor: const Color.fromARGB(255, 49, 68, 77),
+
       ),
     );
   }
