cfd3e94477ee92926555e87d0b742a18b674b170
longmaker2 <l.deng@alustudent.com>
2024-01-30 19:09:21 +0300

    trying some crazy things on the landing page

--- flavour_fiesta/lib/screens/onboarding_screens/landing_screen.dart
+++ flavour_fiesta/lib/screens/onboarding_screens/landing_screen.dart
@@ -4,30 +4,42 @@ class LandingPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'Finger Licking Good!',
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [
+              Color(0xFFFF191C),
+              Color(0xFFFC7B8A),
+            ],
+            stops: [0.0065, 1.0718],
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Finger Licking Good!',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
               ),
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                // Handle button press, navigate to the next screen, etc.
-              },
-              style: ElevatedButton.styleFrom(
-                primary: Colors.white,
-                onPrimary: Colors.black,
-                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.white,
+                  onPrimary: Colors.black,
+                  padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                ),
+                child: Text('Get Started'),
               ),
-              child: Text('Get Started'),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
