b24c1d8fbb67f60cbfb364fc0d23b90cf3fdafa9
RuthBiney <r.biney@alustudent.com>
2024-02-26 14:43:10 +0200

    models modified

--- flavour_fiesta/lib/models/food.dart
+++ flavour_fiesta/lib/models/food.dart
@@ -9,6 +9,33 @@ class FoodItem {
     required this.imagePath,
     required this.price,
     required this.description,
+  });
+}
 
+class PopularItem {
+  final String title;
+  final String imagePath;
+  final double price;
+
+  const PopularItem({
+    required this.title,
+    required this.imagePath,
+    required this.price,
+  });
+}
+
+class PromotionsItem {
+  final String title;
+  final String subtitle;
+  final String description;
+  final double price;
+  final String imagePath;
+
+  const PromotionsItem({
+    required this.title,
+    required this.subtitle,
+    required this.description,
+    required this.price,
+    required this.imagePath,
   });
 }
