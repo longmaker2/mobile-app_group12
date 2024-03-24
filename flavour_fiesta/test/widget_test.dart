87c4fed26b9a23685b0ce810c81519bcf1e6ce4b
kaybrian <kaybrian>
2024-03-24 17:23:08 +0200

    test register and logins

--- flavour_fiesta/test/widget_test.dart
+++ flavour_fiesta/test/widget_test.dart
@@ -5,12 +5,22 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:flavour_fiesta/main.dart';
 
+import 'mock.dart';
+
 void main() {
+
+   setupFirebaseAuthMocks();
+
+  setUpAll(() async {
+    await Firebase.initializeApp();
+  });
+  
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const MyApp());
